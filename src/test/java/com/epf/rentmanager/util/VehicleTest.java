0e7e7d375825f37ec058ad292a86267c178609aa
Deryne <60694751+Deryne@users.noreply.github.com>
2022-03-10 22:20:26 +0100

    Update VehicleTest.java

--- src/test/java/com/epf/rentmanager/util/VehicleTest.java
+++ src/test/java/com/epf/rentmanager/util/VehicleTest.java
@@ -11,16 +11,12 @@ public class VehicleTest {
 	
     @Test
     void return_true_when_number_of_seat_is_correct() {
-        // Given
         Vehicle CorrectVehicle = new Vehicle(1, "Citroen", 4);
-        // Then
         assertTrue(CorrectVehicle.Nb_placesValide());
     }
     @Test
     void return_false_when_number_of_seat_is_correct() {
-        // Given
     	Vehicle IncorrectVehicle  = new Vehicle(1, "Citroen", 1);
-        // Then
         assertFalse(IncorrectVehicle.Nb_placesValide());
     }
 
