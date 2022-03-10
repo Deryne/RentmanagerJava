1d230eb35afb1d5b981c05413141fa02d56c5340
Deryne <60694751+Deryne@users.noreply.github.com>
2022-03-10 22:20:03 +0100

    Update ReservationTest.java

--- src/test/java/com/epf/rentmanager/util/ReservationTest.java
+++ src/test/java/com/epf/rentmanager/util/ReservationTest.java
@@ -12,19 +12,15 @@ import com.epf.rentmanager.model.Reservation;
 public class ReservationTest {
 	@Test
     void Duree_should_return_true_when_reservation_is_too_long() {
-        // Given
         Reservation legalReservation = new Reservation(1 , 1, 1, LocalDate.parse("2000-03-01"),
                 LocalDate.parse("2000-03-11"));
-        // Then
         assertTrue(legalReservation.ResaTropLongue());
     }
 
     @Test
     void Duree_should_return_false_when_reservation_is_correct() {
-        // Given
         Reservation legalReservation = new Reservation(1 ,1, 1, LocalDate.parse("2000-03-01"),
                 LocalDate.parse("2000-03-06"));
-        // Then
         assertFalse(legalReservation.ResaTropLongue());
     }
 
